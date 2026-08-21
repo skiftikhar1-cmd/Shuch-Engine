69b0fe57c8aea7a18bd8514d85bdb5843db0236b
skiftikhar1-cmd <skiftikhar1@gmail.com>
2026-08-21 17:51:15 +0600

    Update config.js

--- Rise-From-Broken-Search-Engine-V1/config.js
+++ Rise-From-Broken-Search-Engine-V1/config.js
@@ -1,7 +1,16 @@
-// RISE FROM BROKEN configuration
+// ============================================
+// RISE FROM BROKEN CONFIGURATION
+// ============================================
 
 window.RISE_FROM_BROKEN_CONFIG = {
-  SUPABASE_URL: "https://vpiywehlgbhklhxecthq.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_MOlWYzq1Df_9w_KWhFo8aA_AT_VZSGw",
-  SEARCH_TABLE: "search_documents"
+
+    SUPABASE_URL:
+        "https://vpiywehlgbhklhxecthq.supabase.co",
+
+    SUPABASE_ANON_KEY:
+        "sb_publishable_MOlWYzq1Df_9w_KWhFo8aA_AT_VZSGw",
+
+    SEARCH_TABLE:
+        "search_documents"
+
 };
