29ad44a7d6515bcba51ebf2cbb99d387cec3b7f0
skiftikhar1-cmd <skiftikhar1@gmail.com>
2026-08-21 18:07:46 +0600

    Update config.js

--- Rise-From-Broken-Search-Engine-V1/config.js
+++ Rise-From-Broken-Search-Engine-V1/config.js
@@ -1,15 +1,19 @@
 // ============================================
-// RISE FROM BROKEN CONFIGURATION
+// RISE FROM BROKEN
+// CONFIGURATION
 // ============================================
 
 window.RISE_FROM_BROKEN_CONFIG = {
 
+    // Supabase Project URL
     SUPABASE_URL:
         "https://vpiywehlgbhklhxecthq.supabase.co",
 
+    // Supabase Publishable / Anon Key
     SUPABASE_ANON_KEY:
         "sb_publishable_MOlWYzq1Df_9w_KWhFo8aA_AT_VZSGw",
 
+    // Database table
     SEARCH_TABLE:
         "search_documents"
 
